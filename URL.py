57370014a3b8e5921f8a5cc267757b5b3aecd4c0
Dmitriy877 <dmitriybukin05@gmail.com>
2024-12-24 20:51:28 +0300

    Step 3

--- URL.py
+++ URL.py
@@ -10,15 +10,26 @@ def main():
 
 	load_dotenv()
 	apikey = os.getenv("VK_API_KEY")
+	url = "vk.cc/cvPDMl"
 
+	payload = {"access_token":apikey,
+	 "v":5.199,
+	 "url":"dvmn.org/modules "
+	 }
 
-	payload = {"access_token":apikey, "v":5.199}
-
-	time_url = 'https://api.vk.ru/method/utils.getServerTime'
+	time_url = 'https://api.vk.ru/method/utils.getShortLink'
 	response = requests.get(time_url, params=payload)
 	response.raise_for_status()
 	print(response.text)
 
+
+
+
+
+	# time_url = 'https://api.vk.ru/method/utils.getServerTime'
+	# response = requests.get(time_url, params=payload)
+	# response.raise_for_status()
+	# print(response.text)
 
 
 	# payload = {"text": "python"}
