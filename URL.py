d02de6ddd350e581c22f396a78e84ae0b5da6480
Dmitriy877 <dmitriybukin05@gmail.com>
2025-01-15 21:50:17 +0300

    Correct mistakes

--- URL.py
+++ URL.py
@@ -16,7 +16,6 @@ def shorten_link(token, url):
 	response.raise_for_status()
 	return response.json()["response"]["short_url"]
 
-
 def count_clicks(token, link):
 	
 	payload_click = {"access_token":token,
@@ -31,68 +30,41 @@ def count_clicks(token, link):
 	response.raise_for_status()
 	return response.json()["response"]["stats"][0]["views"]
 
+def is_shorten_link(url, token):
 
-def is_shorten_link(url):
+	payload = {"access_token":token,
+	 "v":5.199,
+	 "url": url
+	 }
+	link_url = 'https://api.vk.ru/method/utils.getShortLink'
 
-	return urlparse(url)
-
-	# payload_click = {"access_token":token,
-	#  "v":5.199,
-	#  "key": link,
-	#  "interval": "forever",
-	#  "extended": 0
-	#  }
-
-	# click_url = 'https://api.vk.ru/method/utils.getLinkStats'
-	# response = requests.get(click_url, params=payload_click)
-	# response.raise_for_status()
-	# return response.json()
-
-	# parsed = urlparse(url)
-
-	# if len(parsed[2]) <=7:
-	# 	return True
-	# else:
-	# 	return False
-
+	try:
+		response = requests.get(link_url, params=payload)
+		if "short_url" in response.json()["response"]:
+			return False
+	except KeyError:
+		return True
 
 def main():
 
 	load_dotenv()
-	token = os.getenv("VK_API_KEY")
+	token = os.environ["VK_API_KEY"]
 	url = input("Введите ссылку: ")
 
-	is_shorten_link_url =  urlparse(url)[2]
-	is_shorten_link_url_edit = is_shorten_link_url[1:]
-	link = is_shorten_link_url_edit
+	try:
 
-	print(shorten_link(token, url))
+		if not is_shorten_link(url, token):
+			short_link = shorten_link(token, url)
+			print("Сокращенная ссылка", short_link)
+		else:
+			scheme, netloc, path, params, query, fragments = urlparse(url)
+			link = path [1:]
+			click_amount = count_clicks(token, link)
+			print("Количество переходов по ссылке: ", click_amount)
 
+	except KeyError:
 
-
-	# print(count_clicks(token, link))
-
-
-
-
-
-
-
-	# try:
-
-	# 	if is_shorten_link(url) == False:
-	# 		short_link = shorten_link(token, url)["response"]["short_url"]
-	# 		print("Сокращенная ссылка", short_link)
-	# 	else:
-	# 		is_shorten_link_url =  urlparse(url)[2]
-	# 		is_shorten_link_url_edit = is_shorten_link_url[1:]
-	# 		link = is_shorten_link_url_edit
-	# 		click_amount = count_clicks(token, link)["response"]["stats"][0]["views"]
-	# 		print("Количество переходов по ссылке: ", click_amount)
-
-	# except KeyError:
-
-	# 	print("Некорректный URL!")
+		print("Вы ввели неправильную ссылку!")
 
 if __name__ == '__main__':
     main()
